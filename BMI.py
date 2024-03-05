9a76d784aa2a8c381dc0950ea4c4b1de1f78ab24
tiennhat <thespycia2015@gmai.com>
2024-03-05 21:51:28 +0700

    thay doi hai dong cho dieu kien if else BMI

--- BMI.py
+++ BMI.py
@@ -1,34 +1,17 @@
 def tinh_bmi(chieu_cao, can_nang):
-  """
-  Hàm tính chỉ số BMI
-
-  Args:
-      chieu_cao (float): Chiều cao (m)
-      can_nang (float): Cân nặng (kg)
-
-  Returns:
-      float: Chỉ số BMI
-  """
+ 
   return can_nang / (chieu_cao * chieu_cao)
 
 def phan_loai_bmi(bmi):
-  """
-  Hàm phân loại BMI
-
-  Args:
-      bmi (float): Chỉ số BMI
-
-  Returns:
-      str: Phân loại BMI
-  """
+ 
   if bmi < 18.5:
     return "Thiếu cân lắm á nha ăn nhiều zô á nè"
   elif bmi < 25:
-    return "Bình thường"
+    return "chuẩn rồi đó nha"
   elif bmi < 30:
     return "Thừa cân"
   else:
-    return "Béo phì"
+    return "body bạn như Lê Thành Nhật"
 
 chieu_cao = float(input("Nhập số đo chiều cao (m): "))
 can_nang = float(input("Nhập cân nặng (kg): "))
